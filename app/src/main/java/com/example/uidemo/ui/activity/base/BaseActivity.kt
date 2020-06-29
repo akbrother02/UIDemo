1fe395ce04810e52d2b06ced7c248b9d92b43849
Ashish Kumar <kumar.ashish2@religare.com>
2020-06-29 10:37:01 +0530

    Project Setup

---
+++ app/src/main/java/com/example/uidemo/ui/activity/base/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.example.uidemo.ui.activity.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BaseActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
